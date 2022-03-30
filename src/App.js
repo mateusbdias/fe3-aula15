6e3dd0d5b44120dd4bb10621b390468c09a0052a
Mateus Benites Dias <imattdias@gmail.com>
2022-03-30 15:58:27 -0300

    CTD - Front End III - Aula 15 - Exercício Sync

--- src/App.js
+++ src/App.js
@@ -1,23 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { FiEdit, FiTrash } from "react-icons/fi";
+
+import "./styles.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [date, setDate] = useState("");
+
+  useEffect(() => {
+    getTodos()
+  }, []);
+
+  async function getTodos() {
+    setLoading(true);
+    try {
+      const data = await fetch('http://localhost:3000/api/todo');
+      const { todos } = await data.json();
+      setTodos(todos);
+    }
+    catch (error) {
+      alert("Houve um erro ao comunicar com o servidor");
+    }
+    setLoading(false);
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    if (!title || !description || !date) {
+      alert('Preencha todos os campos');
+    } else {
+      const body = {
+        title,
+        description,
+        date,
+        status: false
+      };
+      try {
+        await fetch("http://localhost:3000/api/todo", {
+          method: "POST",
+          body: JSON.stringify(body),
+        });
+        alert("Cadastrado com sucesso");
+        getTodos();
+      }
+      catch (error) {
+        alert("Erro ao cadastrar to-do");
+      }
+    }
+  }
+
+  async function deleteTodo(id) {
+    try {
+      await fetch(`http://localhost:3000/api/todo/${id}`, {
+        method: "DELETE",
+      });
+      alert("To-do deletada com sucesso");
+      getTodos();
+    }
+    catch (error) {
+      alert("Erro ao deletar to-do");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <form onSubmit={handleSubmit}>
+        <h1>ToDo List - 2.0</h1>
+        <div className="inputs">
+          <label>
+            <span>Título</span>
+            <input 
+              placeholder="Titulo" 
+              value={title} 
+              onChange={(event) => setTitle(event.target.value)} 
+            />
+          </label>
+          <label>
+            <span>Descrição</span>
+            <input 
+              placeholder="Descrição" 
+              value={description}
+              onChange={(event) => setDescription(event.target.value)}
+            />
+          </label>
+          <div className="row">
+            <label>
+              <span>Data</span>
+              <input 
+                type="date" 
+                placeholder="Data" 
+                value={date}
+                onChange={(event) => setDate(event.target.value)}
+              />
+            </label>
+          </div>
+        </div>
+        <div className="container-buttons">
+          <button type="submit">Salvar</button>
+          <button>Limpar</button>
+        </div>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li>
+            <div>
+              <h2>{todo.title}</h2>
+              <p>{todo.description}</p>
+              <p>{todo.date}</p>
+            </div>
+            <div className="container-buttons">
+              <FiEdit size={20} color="#444" />
+              <FiTrash 
+                size={20} 
+                color="#444" 
+                onClick={() => deleteTodo(todo.id)} 
+              />
+            </div>
+          </li>
+        ))}
+      </ul>
+      {loading && <h3>Carregando dados...</h3>}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Server, Model } from "miragejs";
+
+import App from "./App";
+
+new Server({
+  models: {
+    todo: Model,
+  },
+  seeds(server) {
+    server.create("todo", {
+      title: "Reunião",
+      description: "Reunião sobre frontend",
+      status: false,
+      date: new Date(),
+    });
+    server.create("todo", {
+      title: "Codificação",
+      description: "Implementação do Carousel animado",
+      status: false,
+      date: new Date(),
+    });
+    server.create("todo", {
+      title: "Testes de Software",
+      description: "Testar a funcionalidade de login e logout",
+      status: false,
+      date: new Date(),
+    });
+  },
+  routes() {
+    this.namespace = "api";
+    this.timing = 50;
+
+    this.get("/todo", (schema) => {
+      return schema.todos.all();
+    });
+
+    this.post("/todo", (schema, request) => {
+      const attrs = JSON.parse(request.requestBody);
+      return schema.todos.create(attrs);
+    });
+
+    this.patch("/todo/:id", (schema, request) => {
+      let id = request.params.id;
+      let todo = JSON.parse(request.requestBody);
+      return schema.db.todos.update(id, todo);
+    });
+
+    this.delete("/todo/:id", (schema, request) => {
+      let id = request.params.id;
+      return schema.todos.find(id).destroy();
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
